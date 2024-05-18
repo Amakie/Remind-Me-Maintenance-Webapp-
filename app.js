c3d70b3227bc9c811c27fc1ec1d8bed95b39ed25
Constance Amatey <consamate57@gmail.com>
2024-05-18 22:32:37 +0000

    defining database content intake

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
+const routes = require('./routes/routesapp')
+
 require('dotenv').config()
 
 const app = express();
@@ -10,5 +12,7 @@ mongoose
 .connect(process.env.MONGODB_URL)
 .then(()=> console.log('connected to MongoDB...'))
 .catch((err) => console.log(err))
+
+app.use(routes)
 
 app.listen(PORT, () => console.log('Listening on: ${PORT}'));
