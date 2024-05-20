dfbbf308069a5a68f120b70ea967a753bed95d70
esianyo <esianicd@gmail.com>
2024-05-20 21:44:44 +0000

    POST request and data saving implemented successfully

--- controllers/controllersapp.js
+++ controllers/controllersapp.js
@@ -16,3 +16,19 @@ module.exports.save_App = async (req, res) => {
         res.send(data)
     })
 }
+
+module.exports.update_App = async (req, res) => {
+    const {_id, text} = req.body
+    modelsapp
+    .findByIdAndUpdate(_id, {text})
+    .then(()=> res.send("Successfully updated"))
+    .catch((err) => console.log(err))
+}
+
+module.exports.delete_App = async (req, res) => {
+    const {_id} = req.body
+    modelsapp
+    .findByIdAndDelete(_id)
+    .then(()=> res.send("Successfully deleted"))
+    .catch((err) => console.log(err))
+}
