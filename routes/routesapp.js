a704bf84eea726bf31179548b24a4cc0f1163ca7
esianyo <esianicd@gmail.com>
2024-05-21 22:56:37 +0000

    Debugging, routes for delete and update.

--- routes/routesapp.js
+++ routes/routesapp.js
@@ -1,9 +1,11 @@
 const {Router} = require ("express")
-const { get_App, save_App } = require ("../controllers/controllersapp")
+const { get_App, save_App, update_App, delete_App } = require ("../controllers/controllersapp")
 
 const router = Router()
 
 router.get('/', get_App)
 router.post('/save', save_App)
+router.post('/update', update_App)
+router.post('/delete', delete_App)
 
 module.exports = router
