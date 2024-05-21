113999102e48a3b8c187c55314c8b6a5bec5f720
Jonah <Jonahemmanuel54@gmail.com>
2024-05-21 11:30:59 +0100

    changing react port and editing footer note

--- client/src/Footer/Footer.js
+++ client/src/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer(){
     return (
         <footer className="bg-gray-800 text-white py-4 px-6 text-center">
-            <p>&copy; 2024 my App. All rights reserved</p>
+            <p>&copy; 2024 Remind Me. All rights reserved</p>
         </footer>
     );
 }
